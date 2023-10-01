c09ce11f9623d5145651f1dda9a80929b4667766
Miguel Angel Perez Garcia <miguelangel.perez.dev@gmail.com>
2023-10-01 14:29:16 +0100

    ref(password): move methods in call order and format code for better read

--- src/main/java/org/password_validator/Password.java
+++ src/main/java/org/password_validator/Password.java
@@ -5,26 +5,27 @@ import java.util.regex.Pattern;
 public class Password {
 
     public boolean isValid(String input) {
-        return hasCapitalLetter(input) && hasValidLength(input) && hasLowerCaseLetter(input) && hasNumber(input) && hasUnderscore(input);
+        return hasValidLength(input) && hasCapitalLetter(input) &&
+                hasLowerCaseLetter(input) && hasNumber(input) && hasUnderscore(input);
     }
 
-    private static boolean hasUnderscore(String input) {
-        return Pattern.matches(".*_.*", input);
-    }
-
-    private static boolean hasNumber(String input) {
-        return Pattern.matches(".*\\d.*", input);
-    }
-
-    private static boolean hasLowerCaseLetter(String input) {
-        return Pattern.matches(".*[a-z].*", input);
+    private static boolean hasValidLength(String input) {
+        return input.length() > 8;
     }
 
     private static boolean hasCapitalLetter(String input) {
         return Pattern.matches(".*[A-Z].*", input);
     }
 
-    private static boolean hasValidLength(String input) {
-        return input.length() > 8;
+    private static boolean hasLowerCaseLetter(String input) {
+        return Pattern.matches(".*[a-z].*", input);
+    }
+
+    private static boolean hasNumber(String input) {
+        return Pattern.matches(".*\\d.*", input);
+    }
+
+    private static boolean hasUnderscore(String input) {
+        return Pattern.matches(".*_.*", input);
     }
 }
